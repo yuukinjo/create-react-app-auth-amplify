a9e74c9421129d3616cb8799ea6e03d10f0fd315
yuukinjo <yuukinjo@ryucom.co.jp>
2022-12-27 16:33:19 +0900

    試しに変更

--- src/App.js
+++ src/App.js
@@ -13,18 +13,9 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <h1>Sample App</h1>
+          <p>これはサンプルで作成したアプリです。</p>
       </div>
     );
   }
